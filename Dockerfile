872a1fc3f47a02d0d4d95e0face1d8e38aeb3dec
Fabricio Veronez <fabricioveronez@gmail.com>
2021-02-04 23:30:11 -0300

    Mudando para o flask plus

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,24 @@
-FROM python:3.7.3-slim
-WORKDIR /app
-COPY ./requirements.txt ./requirements.txt
-RUN pip3 install -r ./requirements.txt
-COPY . .
+# For more information, please refer to https://aka.ms/vscode-docker-python
+FROM python:3.8-slim-buster
+
 EXPOSE 5000
-RUN chmod 777 ./gunicorn_starter.sh
-CMD ["./gunicorn_starter.sh"]
+
+# Keeps Python from generating .pyc files in the container
+ENV PYTHONDONTWRITEBYTECODE=1
+
+# Turns off buffering for easier container logging
+ENV PYTHONUNBUFFERED=1
+
+# Install pip requirements
+COPY requirements.txt .
+RUN python -m pip install -r requirements.txt
+
+WORKDIR /app
+COPY . /app
+
+# Switching to a non-root user, please refer to https://aka.ms/vscode-docker-python-user-rights
+RUN useradd appuser && chown -R appuser /app
+USER appuser
+
+# During debugging, this entry point will be overridden. For more information, please refer to https://aka.ms/vscode-docker-python-debug
+CMD ["gunicorn", "--bind", "0.0.0.0:5000", "app:app"]
--- app.py
+++ app.py
@@ -1,23 +1,27 @@
-import flask
-from flask import request, jsonify
+from flask import Flask, request
+from flask_restplus import Resource, Api
+from flask import jsonify
 
-app = flask.Flask(__name__)
+
+app = Flask(__name__)
+api = Api(app)
 # app.config["DEBUG"] = True
+
+@api.route('/celsius/<int:temperatura>/fahrenheit')
+class ConversaoCelsius(Resource):
+    def get(self, temperatura):
+        fahrenheit = (temperatura * 9 / 5) + 32
+        return jsonify({ 'fahrenheit': fahrenheit })
+
+@api.route('/fahrenheit/<int:temperatura>/celsius')
+class ConversaoFahrenheit(Resource):
+    def get(self, temperatura):
+        celsius = (temperatura - 32) * 5 / 9
+        return jsonify({ 'celsius': celsius })     
 
 @app.route('/', methods=['GET'])
 def home():
-    return "Api de Conversão em Python-v2"
-
-
-@app.route('/celsius/<int:temperatura>/fahrenheit', methods=['GET'])
-def celsius_fahrenheit(temperatura=None):
-    fahrenheit = (temperatura * 9 / 5) + 32
-    return jsonify({ 'fahrenheit': fahrenheit })
-
-@app.route('/fahrenheit/<int:temperatura>/celsius', methods=['GET'])
-def fahrenheit_celsius(temperatura=None):
-    celsius = (temperatura - 32) * 5 / 9
-    return jsonify({ 'celsius': celsius })
+    return "Api de Conversão em Python"
 
 if __name__ == '__main__':
     app.run()
